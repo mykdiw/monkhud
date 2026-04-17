559b287e64311f0746f34c2a67af870c59fdc560
Mayank Dwivedi <56462048+mykdiw@users.noreply.github.com>
2026-04-17 23:39:15 +0530

    Fix next config and npmrc

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,9 +8,6 @@ const nextConfig: NextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
     ],
-  },
-  experimental: {
-    typedRoutes: true,
   },
 }
 
